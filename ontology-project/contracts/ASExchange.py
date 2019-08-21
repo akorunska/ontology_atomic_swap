9afd7dd2352fcfe778c7b4a085c4802c28437d7e
Anna Korunska <akorunska@gmail.com>
2019-08-21 13:41:22 +0300

    set up punica project, simple contract, wrapper and test

---
+++ ontology-project/contracts/ASExchange.py
@@ -0,0 +1,11 @@
+from ontology.interop.System.Runtime import GetTime, CheckWitness, Log, Notify, Serialize, Deserialize
+from ontology.interop.System.Storage import Put, Get, GetContext
+from ontology.builtins import *
+
+
+def Main(operation, args):
+    if operation == 'intiate_order':
+        pass
+
+def intiate_order():
+    pass
---
+++ ontology-project/test/test_ASExchange.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import unittest
+import ASEXchangeWrapper
+
+
+class TestCompiler(unittest.TestCase):
+    def test_initiate_order(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
