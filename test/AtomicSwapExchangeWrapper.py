b7ffe3a0b9dc05e9c64af9e8307f98f71aaa9184
Anna Korunska <akorunska@gmail.com>
2019-10-01 17:17:02 +0300

    added test for data saving

--- test/AtomicSwapExchangeWrapper.py
+++ test/AtomicSwapExchangeWrapper.py
@@ -1,5 +1,6 @@
 from punica.invoke.invoke_contract import Invoke
 from punica.test.test import Test
+from ontology.utils.util import parse_neo_vm_contract_return_type_integer, parse_neo_vm_contract_return_type_string
 import WalletWrapper
 import SdkUtils
 from Utils import GetDeployedContractAddress, FormatOntIdParam
@@ -23,10 +24,35 @@ def initiate_order(amountOfOntToSell, amountOfEthToBuy, hashlock):
     abiFunction = Invoke.get_function(params, 'intiate_order', abi_info)
     return SdkUtils.SendTransaction(contract_address, alice, alice, gas_limit, gas_price, abiFunction, preExec)
 
+def get_amount_of_ont_to_sell(hashlock):
+    preExec = False
+    params = dict()
+    params["order_id"] = "String:" + hashlock
+    abiFunction = Invoke.get_function(params, 'get_amount_of_ont_to_sell', abi_info)
+    responce = SdkUtils.SendTransaction(contract_address, alice, alice, gas_limit, gas_price, abiFunction, preExec)
+    return parse_neo_vm_contract_return_type_string(responce)
+    
+def get_amount_of_eth_to_buy(hashlock):
+    preExec = False
+    params = dict()
+    params["order_id"] = "String:" + hashlock
+    abiFunction = Invoke.get_function(params, 'get_amount_of_eth_to_buy', abi_info)
+    responce = SdkUtils.SendTransaction(contract_address, alice, alice, gas_limit, gas_price, abiFunction, preExec)
+    return parse_neo_vm_contract_return_type_integer(responce)
+
+def get_hashlock(hashlock):
+    preExec = False
+    params = dict()
+    params["order_id"] = "String:" + hashlock
+    abiFunction = Invoke.get_function(params, 'hashlock', abi_info)
+    responce = SdkUtils.SendTransaction(contract_address, alice, alice, gas_limit, gas_price, abiFunction, preExec)
+    return parse_neo_vm_contract_return_type_integer(responce)
+
 def get_orders():
     preExec = True
     params = dict()
     abiFunction = Invoke.get_function(params, 'get_orders', abi_info)
-    return SdkUtils.SendTransaction()
+    responce = SdkUtils.SendTransaction()
+    return responce
 
 
--- test/test_AtomicSwapExchange.py
+++ test/test_AtomicSwapExchange.py
@@ -6,26 +6,27 @@ import AtomicSwapExchangeWrapper
 import SdkUtils
 import random
 import string
-from datetime import datetime
+import time
 
 
 def randomString(stringLength=20):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase + string.ascii_uppercase
-    return str(datetime.now()) + ''.join(random.choice(letters) for i in range(stringLength))
+    return str(time.time()) + ''.join(random.choice(letters) for i in range(stringLength))
 
 
 class TestCompiler(unittest.TestCase):
     def test_initiate_order_new_hashlock(self):
         hashlock = randomString()
-        print(hashlock)
         amountOfOntToSell = 100
         amountOfEthToBuy = 2
-        AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSell, amountOfEthToBuy, hashlock)
+        try:
+            AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSell, amountOfEthToBuy, hashlock)
+        except Exception as e:
+            self.fail(e)
 
     def test_initiate_order_existing_hashlock(self):
         hashlock = randomString()
-        print(hashlock)
         amountOfOntToSellInitial = 100
         amountOfEthToBuyInitial = 2
         AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSellInitial, amountOfEthToBuyInitial, hashlock)
@@ -36,7 +37,16 @@ class TestCompiler(unittest.TestCase):
             AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSellUpdated, amountOfEthToBuyUpdated, hashlock)
 
     def test_initiate_order_data_is_saved(self):
-        self.assertFalse(True)
+        hashlock = randomString()
+        amountOfOntToSell = 100
+        amountOfEthToBuy = 2
+        AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSell, amountOfEthToBuy, hashlock)
+        savedAmountOfOntToSell = AtomicSwapExchangeWrapper.get_amount_of_ont_to_sell(hashlock)
+        savedAmountOfEthToBuy = AtomicSwapExchangeWrapper.get_amount_of_eth_to_buy(hashlock)
+        print(savedAmountOfOntToSell)
+        print(savedAmountOfEthToBuy)
+        self.assertEqual(savedAmountOfOntToSell, amountOfOntToSell)
+        self.assertEqual(savedAmountOfEthToBuy, amountOfEthToBuy)
 
 
 if __name__ == '__main__':
