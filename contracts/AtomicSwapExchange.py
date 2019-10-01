802f7595e803c04bbd53d2ac3bcd63ac8b41206d
Anna Korunska <akorunska@gmail.com>
2019-10-01 17:17:20 +0300

    added code for data saving

--- contracts/AtomicSwapExchange.py
+++ contracts/AtomicSwapExchange.py
@@ -16,12 +16,32 @@ def Main(operation, args):
         eth_to_buy = args[1]
         hashlock = args[2]
         return intiate_order(ont_to_sell, eth_to_buy, hashlock)
+    if operation == 'get_amount_of_ont_to_sell':
+        hashlock = args[0]
+        return get_amount_of_ont_to_sell(hashlock)
+    if operation == 'get_amount_of_eth_to_buy':
+        hashlock = args[0]
+        return get_amount_of_eth_to_buy(hashlock)
+    if operation == 'get_hashlock':
+        hashlock = args[0]
+        return get_hashlock(hashlock)
 
 
 def intiate_order(ont_to_sell, eth_to_buy, hashlock):
-    exchange_id = hashlock
-    if len(Get(context, ConcatKey(exchange_id, HASH))) != 0:
+    order_id = hashlock
+    if len(Get(context, ConcatKey(order_id, HASH))) != 0:
         Revert()
-    Put(context, ConcatKey(exchange_id, HASH), hashlock)
+    Put(context, ConcatKey(order_id, HASH), hashlock)
+    Put(context, ConcatKey(order_id, ETH_TO_BUY), eth_to_buy)
+    Put(context, ConcatKey(order_id, ONT_TO_SELL), ont_to_sell)
+
+def get_amount_of_ont_to_sell(order_id):
+    return Get(context, ConcatKey(order_id, ETH_TO_BUY))
+
+def get_amount_of_eth_to_buy(order_id):
+    return Get(context, ConcatKey(order_id, ONT_TO_SELL))
+
+def get_hashlock(order_id):
+    return Get(context, ConcatKey(order_id, HASH))
     
     
