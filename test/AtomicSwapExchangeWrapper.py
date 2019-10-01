d622ba6ca8f37dbc63c312975e86e005ab36555c
Anna Korunska <akorunska@gmail.com>
2019-10-01 15:14:08 +0300

    using gasprice constant + starting random hashlock string with timestamp data

--- test/AtomicSwapExchangeWrapper.py
+++ test/AtomicSwapExchangeWrapper.py
@@ -1,12 +1,11 @@
 from punica.invoke.invoke_contract import Invoke
 from punica.test.test import Test
-from ontology.utils.util import parse_neo_vm_contract_return_type_integer, parse_neo_vm_contract_return_type_string, parse_neo_vm_contract_return_type_bool
 import WalletWrapper
 import SdkUtils
 from Utils import GetDeployedContractAddress, FormatOntIdParam
 
 
-gas_limit = 500000
+gas_limit = SdkUtils.g_gasLimit
 gas_price = 0
 abi_path = './contracts/build/AtomicSwapExchange_abi.json'
 contract_address = bytes.fromhex(GetDeployedContractAddress("AtomicSwapExchange"))
--- test/test_AtomicSwapExchange.py
+++ test/test_AtomicSwapExchange.py
@@ -6,11 +6,13 @@ import AtomicSwapExchangeWrapper
 import SdkUtils
 import random
 import string
+from datetime import datetime
 
 
 def randomString(stringLength=20):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase + string.ascii_uppercase
+    random.seed(datetime.now())
     return ''.join(random.choice(letters) for i in range(stringLength))
 
 
@@ -25,15 +27,17 @@ class TestCompiler(unittest.TestCase):
     def test_initiate_order_existing_hashlock(self):
         hashlock = randomString()
         print(hashlock)
-        amountOfOntToSell = 100
-        amountOfEthToBuy = 2
-        AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSell, amountOfEthToBuy, hashlock)
+        amountOfOntToSellInitial = 100
+        amountOfEthToBuyInitial = 2
+        AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSellInitial, amountOfEthToBuyInitial, hashlock)
         SdkUtils.WaitNextBlock()
+        amountOfOntToSellUpdated = 200
+        amountOfEthToBuyUpdated = 3
         with self.assertRaises(Exception):
-            AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSell, amountOfEthToBuy, hashlock)
+            AtomicSwapExchangeWrapper.initiate_order(amountOfOntToSellUpdated, amountOfEthToBuyUpdated, hashlock)
 
-    # def test_initiate_order_data_is_saved(self):
-    #     pass
+    def test_initiate_order_data_is_saved(self):
+        self.assertFalse(True)
 
 
 if __name__ == '__main__':
